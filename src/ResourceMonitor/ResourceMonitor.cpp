39171eb920c38350248d5362eed0dc8098745cc1
zhaotianff <zhaotianff@163.com>
2024-06-12 16:48:14 +0800

    update ui

--- src/ResourceMonitor/ResourceMonitor.cpp
+++ src/ResourceMonitor/ResourceMonitor.cpp
@@ -17,9 +17,14 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+VOID CreateTab(HWND,HFONT);
+LRESULT CALLBACK WndProcInfoClass(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 BOOL g_bMonitor = TRUE;
-HWND g_hwndClassTabControl;
-HWND g_hwndMemTabControl;
+HWND g_hwndInfoTabControl;
+HWND g_hwndInfoClassTabControl;
+HWND g_aInfoClassHwnd[2];
+HFONT hFont;
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -132,42 +137,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_CREATE:
     {
         EnableMenuItem(GetMenu(hWnd), IDM_STARTMONITOR, MF_BYCOMMAND | MF_GRAYED);
-        RECT rect;
-        GetClientRect(hWnd, &rect);
-        g_hwndClassTabControl = CreateWindowEx(0, WC_TABCONTROL, NULL, WS_CHILD | WS_VISIBLE | TCS_MULTILINE | TCS_TOOLTIPS, 0, 0, rect.right, rect.bottom, hWnd, NULL, hInst, NULL);
-
-        TCITEM tie;
-        tie.mask = TCIF_TEXT | TCIF_IMAGE;
-        tie.iImage = -1;
-        TCHAR achTemp[256];
-        tie.pszText = achTemp;
-        StringCchCopy(achTemp, _countof(achTemp), L"概览");
-        if (TabCtrl_InsertItem(g_hwndClassTabControl, 0, &tie) == -1)
-        {
-            DestroyWindow(g_hwndClassTabControl);
-            return NULL;
-        }
-
-        TCITEM subTie;
-        subTie.mask = TCIF_TEXT | TCIF_IMAGE;
-        subTie.iImage = -1;
-        TCHAR achSub[256];
-        subTie.pszText = achSub;
-        StringCchCopy(achSub, _countof(achSub), L"内存");
-
-        RECT rectClassTab{};
-        TabCtrl_AdjustRect(g_hwndClassTabControl, FALSE, &rectClassTab);
-
-        g_hwndMemTabControl = CreateWindowEx(0, WC_TABCONTROL, NULL, WS_CHILD | WS_VISIBLE | TCS_VERTICAL | TCS_TOOLTIPS, rectClassTab.left, rectClassTab.top, rect.right, rect.bottom, g_hwndClassTabControl, NULL, hInst, NULL);
-
-        if (TabCtrl_InsertItem(g_hwndMemTabControl, 0, &subTie) == -1)
-        {
-            DestroyWindow(g_hwndMemTabControl);
-            return NULL;
-        }
-
+        CreateTab(hWnd,hFont);
         break;
     }
+    case WM_NOTIFY:
+        switch (((LPNMHDR)lParam)->code)
+        {
+        case TCN_SELCHANGE:
+            auto nIndex = SendMessage(g_hwndInfoClassTabControl, TCM_GETCURSEL, 0, 0);
+            for (int i = 0; i < 2; i++)
+            {
+                ShowWindow(g_aInfoClassHwnd[i], SW_HIDE);
+            }
+            ShowWindow(g_aInfoClassHwnd[nIndex], SW_SHOW);
+            break;
+        }
     case WM_COMMAND:
         {
             int wmId = LOWORD(wParam);
@@ -199,6 +183,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+    case WM_SIZE:
+    {
+        RECT rect{};
+        SetRect(&rect, 0, 0, LOWORD(lParam), HIWORD(lParam));
+        SendMessage(g_hwndInfoClassTabControl, TCM_ADJUSTRECT, FALSE, (LPARAM)&rect);
+        for (int i = 0; i < 2; i++)
+        {
+            MoveWindow(g_aInfoClassHwnd[i],
+                rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, TRUE);
+        }
+        break;
+    }
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
@@ -208,8 +204,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_DESTROY:
+    {
         PostQuitMessage(0);
+        DeleteObject(hFont);
         break;
+    }
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
@@ -234,4 +233,110 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+VOID CreateTab(HWND hWnd,HFONT hFont)
+{
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    g_hwndInfoTabControl = CreateWindowEx(0, WC_TABCONTROL, NULL, WS_CHILD | WS_VISIBLE | TCS_MULTILINE | TCS_TOOLTIPS, 0, 0, rect.right, rect.bottom, hWnd, NULL, hInst, NULL);
+
+    TCITEM tie;
+    tie.mask = TCIF_TEXT | TCIF_IMAGE;
+    tie.iImage = -1;
+    TCHAR achTemp[256];
+    tie.pszText = achTemp;
+    StringCchCopy(achTemp, _countof(achTemp), L"概览");
+    if (TabCtrl_InsertItem(g_hwndInfoTabControl, 0, &tie) == -1)
+    {
+        DestroyWindow(g_hwndInfoTabControl);
+        return;
+    }
+
+    TCITEM subTie;
+    subTie.mask = TCIF_TEXT | TCIF_IMAGE;
+    subTie.iImage = -1;
+    TCHAR achSub[256];
+    subTie.pszText = achSub;
+    StringCchCopy(achSub, _countof(achSub), L"内存");
+
+    RECT rectClassTab{};
+    TabCtrl_AdjustRect(g_hwndInfoTabControl, FALSE, &rectClassTab);
+
+    g_hwndInfoClassTabControl = CreateWindowEx(0, WC_TABCONTROL, NULL, WS_CHILD | WS_VISIBLE | TCS_VERTICAL | TCS_TOOLTIPS, rectClassTab.left, rectClassTab.top, rect.right, rect.bottom, g_hwndInfoTabControl, NULL, hInst, NULL);
+
+    if (TabCtrl_InsertItem(g_hwndInfoClassTabControl, 0, &subTie) == -1)
+    {
+        DestroyWindow(g_hwndInfoClassTabControl);
+        return;
+    }
+
+    subTie.mask = TCIF_TEXT | TCIF_IMAGE;
+    subTie.iImage = -1;
+    subTie.pszText = achSub;
+    StringCchCopy(achSub, _countof(achSub), L"CPU");
+
+    if (TabCtrl_InsertItem(g_hwndInfoClassTabControl, 1, &subTie) == -1)
+    {
+        DestroyWindow(g_hwndInfoClassTabControl);
+        return;
+    }
+
+    WNDCLASSEX wndclass;
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProcInfoClass;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInst;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = TEXT("ClassChildWindow");
+    wndclass.hIconSm = NULL;
+    RegisterClassEx(&wndclass);
+    g_aInfoClassHwnd[0] = CreateWindowEx(0, TEXT("ClassChildWindow"), NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0, g_hwndInfoClassTabControl, NULL, hInst, NULL);
+    g_aInfoClassHwnd[1] = CreateWindowEx(0, TEXT("ClassChildWindow"), NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0, g_hwndInfoClassTabControl, NULL, hInst, NULL);
+
+    auto nIndex = SendMessage(g_hwndInfoClassTabControl, TCM_GETCURSEL, 0, 0);
+    for (int i = 0; i < 2; i++)
+    {
+        ShowWindow(g_aInfoClassHwnd[i],SW_HIDE);
+    }
+    ShowWindow(g_aInfoClassHwnd[nIndex], SW_SHOW);
+
+    hFont = CreateFont(18, 0, 0, 0, 0, 0, 0, 0, GB2312_CHARSET, 0, 0, 0, 0, TEXT("微软雅黑"));
+    SendMessage(g_hwndInfoTabControl, WM_SETFONT, (WPARAM)hFont, FALSE);
+    SendMessage(g_hwndInfoClassTabControl, WM_SETFONT, (WPARAM)hFont, FALSE);
+}
+
+
+LRESULT CALLBACK WndProcInfoClass(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        RECT rect{};
+        GetClientRect(hWnd, &rect);
+        if(hWnd == g_aInfoClassHwnd[0])
+            DrawText(hdc, TEXT("内存"), _tcslen(TEXT("内存")),
+                &rect, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+        else if(hWnd == g_aInfoClassHwnd[1])
+            DrawText(hdc, TEXT("CPU"), _tcslen(TEXT("CPU")),
+                &rect, DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+        EndPaint(hWnd, &ps); 
+        break;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
 }
